61275cea03c77c894fbd4c663641aa8d459e0252
pooja.aug25@gmail.com <pooja.aug25@gmail.com>
2024-04-06 13:39:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,44 +6,4 @@ terraform {
     }
   }
 }
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Create a VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Import ami imagels
-
-resource "aws_ami" "my_ami" {
-  name                = "my-tf-example"
-  virtualization_type = "hvm"
-  root_device_name    = "/dev/xvda"
-  imds_support        = "v2.0" # Enforce usage of IMDSv2. You can safely remove this line if your application explicitly doesn't support it.
-  ebs_block_device {
-    device_name = "/dev/xvda"
-    snapshot_id = "snap-xxxxxxxx"
-    volume_size = 8
-  }
-}
-#Create securituy group
-resource "aws_security_group" "my_sec_group" {
-  name        = "secgroup"
-  description = "this is a security group"
-  vpc_id      = aws_vpc.my_vpc.id
-  tags = {
-    Name = "example"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "my_ingress_rule" {
-  security_group_id = aws_security_group.my_sec_group.id
-  cidr_ipv4   = "10.0.0.0/8"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
+#this is a test git file
