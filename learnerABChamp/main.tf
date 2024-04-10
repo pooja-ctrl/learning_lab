b93232b54a7c8bbfaaa506174a944845687423c5
pooja.aug25@gmail.com <69320185+pooja-ctrl@users.noreply.github.com>
2024-04-10 07:25:00 +0100

    added main tf

---
+++ learnerABChamp/main.tf
@@ -0,0 +1,4 @@
+# Create a VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
--- main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-#this is a test git file
